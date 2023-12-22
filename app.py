11da19b02741d9d6730264af5c08f25d53cba8a0
Mohamed Saeed <mohamedsaeed823@users.noreply.huggingface.co>
2023-12-22 13:11:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from fastai.vision.all import *
 import gradio as gr
 
-gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=2)).launch(share=True)
-
 learn = load_learner('model.pkl')
 labels = learn.dls.vocab
 def predict(img):
     img = PILImage.create(img)
     pred,pred_idx,probs = learn.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=2)).launch(share=True)    
