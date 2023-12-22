9a59b3e980e002e529b10512b54b073bca894fa3
Mohamed Saeed <mohamedsaeed823@users.noreply.huggingface.co>
2023-12-22 12:56:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from fastai.vision.all import *
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
+gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=2)).launch(share=True)
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+learn = load_learner('model.pkl')
+labels = learn.dls.vocab
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
