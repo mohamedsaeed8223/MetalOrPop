b87141fc160e2abeb62c67baea1d5e7e7b19b56f
Mohamed Saeed <mohamedsaeed823@users.noreply.huggingface.co>
2023-12-23 20:20:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from fastai.vision.all import *
 import gradio as gr
 
+def is_metal_album(x): return x[0].isupper()
+
 learn = load_learner('model.pkl')
+
+categories = ('American Pop Album', 'Metal Album')
+
 labels = learn.dls.vocab
+
 def predict(img):
     img = PILImage.create(img)
     pred,pred_idx,probs = learn.predict(img)
-    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+    return dict(zip(categories, map(float,probs)))
 
 gr.Interface(fn=predict, inputs=gr.inputs.Image(shape=(512, 512)), outputs=gr.outputs.Label(num_top_classes=2)).launch(share=True)    
